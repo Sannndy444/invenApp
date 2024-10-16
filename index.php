c82ae6f85a0b5d926159e58cadbf198e218d8c78
unknown <sandyriswantino1933@gmail.com>
2024-10-16 10:46:31 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>InvenApp</title>
+</head>
+<body>
+    helo word
+</body>
+</html>
